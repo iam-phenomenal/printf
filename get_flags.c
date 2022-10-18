2df37fe4852e3b574c6711351e2cfd8a38d9350f
iam-phenomenal <ayandiranibrahim@outlook.com>
2022-10-18 07:07:56 +0100

    Handling flags

--- get_flags.c
+++ get_flags.c
@@ -1,9 +1,10 @@
 #include "main.h"
 
 /**
- * get_flags - Calculates active flags
+ * get_flags - Entry
  * @format: Formatted string in which to print the arguments
  * @i: take a parameter.
+ * Description: Calculates active flags
  * Return: Flags:
  */
 int get_flags(const char *format, int *i)
