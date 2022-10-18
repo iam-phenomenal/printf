ba228b6f16223f8e2e1716293e1ef6bce25eecf2
iam-phenomenal <ayandiranibrahim@outlook.com>
2022-10-18 07:15:20 +0100

    Handling print statement width

--- get_width.c
+++ get_width.c
@@ -1,13 +1,14 @@
 #include "main.h"
 
 /**
- * get_width - Calculates the width for printing
+ * get_width - Entry
  * @format: Formatted string in which to print the arguments.
  * @i: List of arguments to be printed.
  * @list: list of arguments.
- *
- * Return: width.
- */
+ * Description: Calculates the width for printing
+ * Return: width
+*/
+
 int get_width(const char *format, int *i, va_list list)
 {
 	int curr_i;
